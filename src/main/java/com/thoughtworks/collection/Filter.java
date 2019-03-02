152d2a5870d30ef489a8ce7a23b5f01b75cc59bb
XY-ZHOU <731986251@qq.com>
2019-03-02 20:31:07 +0800

    finish functions of filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -7,25 +7,51 @@ import java.util.Map;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : arrayList) {
+            if (i % 2 == 0) {
+                list.add(i);
+            }
+        }
+        return list;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : arrayList) {
+            if (i % 3 == 0) {
+                list.add(i);
+            }
+        }
+        return list;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer num : firstList) {
+            if (secondList.contains(num)) {
+                list.add(num);
+            }
+        }
+        return list;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        HashSet<Integer> set = new HashSet<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : arrayList) {
+            set.add(i);
+        }
+        for (int i : set) {
+            list.add(i);
+        }
+        return list;
     }
 }
