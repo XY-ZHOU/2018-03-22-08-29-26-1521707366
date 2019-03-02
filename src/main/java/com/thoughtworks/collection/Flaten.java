37047eb0ed569ee749c5a5ad5a13c957c92446e1
XY-ZHOU <731986251@qq.com>
2019-03-02 20:31:25 +0800

    finish functions of flaten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -7,15 +7,31 @@ import java.util.List;
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                list.add(array[i][j]);
+            }
+        }
+        return list;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> newList = transformToOneDimesional();
+        Iterator<Integer> iterator = newList.iterator();
+        while (iterator.hasNext()) {
+            Integer num = iterator.next();
+            if (!list.contains(num)) {
+                list.add(num);
+            }
+        }
+        return list;
     }
 }
